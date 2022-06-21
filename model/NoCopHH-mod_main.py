70d701c066ac8b8f18c1afb4c3c7d4cc735a8dc0
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2022-06-21 09:59:16 +0200

    rename folder with the framework

--- model/NoCopHH-mod_main.py
+++ model/NoCopHH-mod_main.py
@@ -7,6 +7,7 @@ import pyomo.environ as py
 import numpy as np
 from datetime import datetime
 import os
+import numpy as np
 
 
 # read in input data and parameters
@@ -37,13 +38,19 @@ solution = solver.solve(model)
 print("Total revenues: %s" % np.round(model.objective(), 0))
 
 _now = datetime.now().strftime("%Y%m%dT%H%M")
-_results_name = "Case_A"
+_results_name = "45Future-100CO2"
 result_dir = os.path.join("result", "{}-{}".format(_results_name, _now))
 if not os.path.exists(result_dir):
     os.makedirs(result_dir)
 
 to_IAMC.write_results_to_ext_iamc_format(model, result_dir)
+total_revenues = model.revenues_spot() + model.revenues_future() + model.revenues_H2() 
+print(model.revenues_spot())
+print(np.round(model.revenues_spot() / total_revenues * 100, 2))
+print(np.round(model.revenues_future() / total_revenues * 100, 2))
+print(np.round(model.revenues_H2() / total_revenues * 100, 2))
+
 # print(model.revenues_spot.get_values())
 # print(model.revenues_future.get_values())
-# print(model.revenues_H2.get_values())
-print(sum(model.v_q_H2.get_values().values()))
+# print(model.revenues_H2())
+# print(sum(model.v_q_H2.get_values().values()))
